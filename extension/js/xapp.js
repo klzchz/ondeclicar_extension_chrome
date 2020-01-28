e7b93aa9f843c473e6da9ff829a94515cac78e58
Lucas Andrade <lucascorporation@live.com>
2020-01-28 14:55:49 -0300

    testes de requisições com axios

--- extension/js/xapp.js
+++ extension/js/xapp.js
@@ -1,12 +1,27 @@
 
-    
+const url  = "http://www.omdbapi.com/?apikey=6fd6c25e";
+
+
 $('#title').append('This Is Us');
 
 $(".card").hide();
 
 $('body').on('click','#search',function(){
+    let data = $('#serie').val();
+    
     $(".card").show();
-    $("#search-box").hide();
+    
+
+    axios.get(`${url}&t=${data}`)
+        .then((result)=>{
+            $("#search-box").hide();
+            alert(result.Body.Title);
+        })
+        .catch((result) => {
+            console.log(result);
+        }).finally(function(){
+                console.log("Terminou a requisição")
+        });
 });
     // $.ajax({
     //     type: "method",
