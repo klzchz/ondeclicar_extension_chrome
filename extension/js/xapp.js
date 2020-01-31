88686890d5e14ce44457ccc2535763832a00fb25
Lucas Andrade <lucascorporation@live.com>
2020-01-31 15:40:39 -0300

    requisição ajax do seriado ou filme

--- extension/js/xapp.js
+++ extension/js/xapp.js
@@ -4,24 +4,44 @@ const url  = "http://www.omdbapi.com/?apikey=6fd6c25e";
 
 $('#title').append('This Is Us');
 
+$("#loader-wrapper").removeAttr("style").hide();
+// $('#loader-wrapper').attr({display:none});
 $(".card").hide();
 
 $('body').on('click','#search',function(){
+    $("#loader-wrapper").show();
     let data = $('#serie').val();
     
-    $(".card").show();
     
+    
+    if(data == '' ||!data)
+         data = 'This+Is+Us';
 
     axios.get(`${url}&t=${data}`)
         .then((result)=>{
-            $("#search-box").hide();
-            alert(result.Body.Title);
+
+            $('#title').html('');
+            $('#poster').attr('src','');
+            $('#description').html('');
+            $('#imdb').html('');
+            $('#global').html('');
+          
+            $('#title').append(result.data.Title);
+            $('#poster').attr('src',result.data.Poster);
+            $('#description').append(result.data.Plot);
+            $('#imdb').append(result.data.imdbRating);
+            $('#global').append(result.data.Ratings[0].Value);
+            // alert(result.data.Title);
+            // conosole.log(response)
         })
         .catch((result) => {
             console.log(result);
-        }).finally(function(){
-                console.log("Terminou a requisição")
-        });
+        })
+        .finally(()=>{
+            $("#loader-wrapper").removeAttr("style").hide()
+            $("#search-box").hide();
+            $(".card").show();
+    });
 });
     // $.ajax({
     //     type: "method",
